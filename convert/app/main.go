626d5907b9fea953a1aa6711ebf55ce0daf11be6
Tuan Tran <minhtuanphc201@gmail.com>
2025-09-16 11:27:36 +0700

    Update main.go

--- convert/app/main.go
+++ convert/app/main.go
@@ -11,7 +11,7 @@ import (
 func handler(ctx context.Context, event events.S3Event) (finished bool, err error) {
 
 	fmt.Printf("----- start -----\n")
-	println("Hello World From lambda Convert!")
+	println("Hello World From lambda Convert From Elbon!")
 	return true, nil
 }
 
